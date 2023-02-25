e59cd1fc3ca00682935b0c9c46e4ebf22343c29a
Ali Pakdel <aliipakdel98@gmail.com>
2023-02-25 17:59:16 +0330

    .gitignore fixed

--- src/main/java/org/Baloot/Main.java
+++ src/main/java/org/Baloot/Main.java
@@ -1,7 +1,14 @@
 package org.Baloot;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world2!");
+//        ObjectMapper mapper = new ObjectMapper();
+//        String user = mapper.readValue("{\"name\": \"John\"}", );
+//
+//        System.out.println(user.name); //John
     }
 }
+

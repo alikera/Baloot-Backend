f328e94b7053816ffe3a66c4e858f6944d62e96e
alikera <alikeramati79@gmail.com>
2023-03-01 16:20:06 +0330

    Test AddToBuyList added

--- src/test/java/RateCommodityTest.java
+++ src/test/java/RateCommodityTest.java
@@ -5,6 +5,7 @@ import org.Baloot.Commodity;
 import org.Baloot.Exception.CommodityNotFoundException;
 import org.Baloot.Exception.ExceptionHandler;
 import org.Baloot.Exception.InvalidRatingException;
+import org.Baloot.Exception.UserNotFoundException;
 import org.Baloot.User;
 import org.junit.*;
 
@@ -18,17 +19,19 @@ public class RateCommodityTest {
     private CommandHandler cmdHandler;
     User user;
     @Before
-    public void setUp() throws JsonProcessingException {
+    public void setUp() throws JsonProcessingException, UserNotFoundException {
         cmdHandler = new CommandHandler();
-
+        cmdHandler.addProvider("{\"id\": 1, \"name\": \"provider1\", \"registryDate\": \"2023-09-15\"}");
         cmdHandler.addUser("{\"username\": \"user1\", \"password\": \"1234\", \"email\": \"user@gmail.com\", " +
                 "\"birthDate\": \"1977-09-15\", \"address\": \"address1\", \"credit\": 1500}");
-        cmdHandler.addCommodity("{\"id\": 1, \"name\": \"Headphone\", \"providerId\": 3, \"price\": 35000, " +
-                "\"categories\": [\"Technology\", \"Phone\"], \"rating\": 8.8, \"inStock\": 50}");
-        cmdHandler.addCommodity("{\"id\": 2, \"name\": \"Apple\", \"providerId\": 2, \"price\": 3000, " +
-                "\"categories\": [\"Technology\"], \"rating\": 1.8, \"inStock\": 20}");
+        cmdHandler.addCommodity("{\"id\": 1, \"name\": \"Headphone\", \"providerId\": 1, \"price\": 35000, " +
+                "\"categories\": [\"Technology\", \"Phone\"], \"rating\": 0.0, \"inStock\": 50}");
+        cmdHandler.addCommodity("{\"id\": 2, \"name\": \"Apple\", \"providerId\": 1, \"price\": 3000, " +
+                "\"categories\": [\"Technology\"], \"rating\": 0.0, \"inStock\": 20}");
         cmdHandler.addCommodity("{\"id\": 3, \"name\": \"Apple\", \"providerId\": 1, \"price\": 300, " +
-                "\"categories\": [\"Fruit\"], \"rating\": 9.8, \"inStock\": 220}");
+                "\"categories\": [\"Fruit\"], \"rating\": 0.0, \"inStock\": 220}");
+
+        user = cmdHandler.findByUsername("user1");
     }
     @After
     public void tearDown() {
@@ -41,7 +44,7 @@ public class RateCommodityTest {
         double ratingsCount = commodity.getCountOfRatings();
         commodity.rateCommodity(user.getUsername(), userGivenRating);
 
-        assertEquals(ratingsCount + 1, commodity.getCountOfRatings());
+        assertEquals(ratingsCount + 1, commodity.getCountOfRatings(), 0.01);
 //        assertEquals((rating + userGivenRating)/(ratingsCount + 1), commodity.getRating());
     }
     @Test
@@ -52,7 +55,7 @@ public class RateCommodityTest {
         commodity.rateCommodity(user.getUsername(), userGivenRating);
 
 //        assertEquals(ratingsCount + 1, commodity.getCountOfRatings());
-        assertEquals((rating + userGivenRating)/commodity.getCountOfRatings(), commodity.getRating());
+        assertEquals((rating + userGivenRating)/commodity.getCountOfRatings(), commodity.getRating(), 0.01);
     }
     @Test
     public void testDontIncreaseRatingsCountIfUserHasAlreadyRatedCurrentCommodity() throws ExceptionHandler {
@@ -63,28 +66,28 @@ public class RateCommodityTest {
         int userGivenRating_2 = 2;
         commodity.rateCommodity(user.getUsername(), userGivenRating_2);
 
-        assertEquals(ratingsCount, commodity.getCountOfRatings());
+        assertEquals(ratingsCount, commodity.getCountOfRatings(), 0.01);
 //        assertEquals((rating + userGivenRating)/(ratingsCount + 1), commodity.getRating());
     }
-
     @Test
     public void testCalculateAverageRatingIfUserHasAlreadyRatedCurrentCommodity() throws ExceptionHandler {
         int userGivenRating_1 = 4;
         Commodity commodity =  cmdHandler.findByCommodityId(1);
         commodity.rateCommodity(user.getUsername(), userGivenRating_1);
 
-        double rating  = commodity.getRating();
+        double rating = commodity.getRating();
+        double ratingsCount = commodity.getCountOfRatings();
+
         int userGivenRating_2 = 2;
         commodity.rateCommodity(user.getUsername(), userGivenRating_2);
 
 //        assertEquals(ratingsCount, commodity.getCountOfRatings());
-        assertEquals((rating + userGivenRating_2)/commodity.getCountOfRatings(), commodity.getRating());
+        assertEquals(((rating*ratingsCount - userGivenRating_1) + userGivenRating_2) / commodity.getCountOfRatings(), commodity.getRating(),0.01);
     }
     @Test
     public void TestErrorIfUserRatingIsNotBetween1To10() throws ExceptionHandler{
         int userGivenRating = -1;
         Commodity commodity =  cmdHandler.findByCommodityId(1);
-        commodity.rateCommodity(user.getUsername(), userGivenRating);
 
 //        assertEquals(ratingsCount + 1, commodity.getCountOfRatings());
         assertThrows(InvalidRatingException.class, () -> commodity.rateCommodity(user.getUsername(), userGivenRating));
--- src/test/java/TestAddToBuyList.java
+++ src/test/java/TestAddToBuyList.java
@@ -3,6 +3,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.Baloot.CommandHandler;
 import org.Baloot.Commodity;
 import org.Baloot.Exception.ExceptionHandler;
+import org.Baloot.Exception.UserNotFoundException;
+import org.Baloot.User;
 import org.junit.*;
 
 import java.util.ArrayList;
@@ -12,20 +14,24 @@ import static org.junit.Assert.*;
 
 public class TestAddToBuyList {
     private CommandHandler cmdHandler;
+    User user;
 
     @Before
-    public void setUp() throws JsonProcessingException {
+    public void setUp() throws JsonProcessingException, UserNotFoundException {
         cmdHandler = new CommandHandler();
+        cmdHandler.addProvider("{\"id\": 1, \"name\": \"provider1\", \"registryDate\": \"2023-09-15\"}");
 
         cmdHandler.addUser("{\"username\": \"user1\", \"password\": \"1234\", \"email\": \"user@gmail.com\", " +
                 "\"birthDate\": \"1977-09-15\", \"address\": \"address1\", \"credit\": 1500}");
 
-        cmdHandler.addCommodity("{\"id\": 1, \"name\": \"Headphone\", \"providerId\": 3, \"price\": 35000, " +
-                "\"categories\": [\"Technology\", \"Phone\"], \"rating\": 8.8, \"inStock\": 0}");
-        cmdHandler.addCommodity("{\"id\": 2, \"name\": \"Apple\", \"providerId\": 2, \"price\": 3000, " +
+        cmdHandler.addCommodity("{\"id\": 1, \"name\": \"Headphone\", \"providerId\": 1, \"price\": 35000, " +
+                "\"categories\": [\"Technology\", \"Phone\"], \"rating\": 8.8, \"inStock\": 10}");
+        cmdHandler.addCommodity("{\"id\": 2, \"name\": \"Apple\", \"providerId\": 1, \"price\": 3000, " +
                 "\"categories\": [\"Technology\"], \"rating\": 1.8, \"inStock\": 20}");
         cmdHandler.addCommodity("{\"id\": 3, \"name\": \"Apple\", \"providerId\": 1, \"price\": 300, " +
-                "\"categories\": [\"Fruit\"], \"rating\": 9.8, \"inStock\": 220}");
+                "\"categories\": [\"Fruit\"], \"rating\": 9.8, \"inStock\": 0}");
+        user = cmdHandler.findByUsername("user1");
+
     }
 
     @After
@@ -34,42 +40,36 @@ public class TestAddToBuyList {
     }
 
     @Test
-    public void testAddToBuyListNotExistedUser() throws JsonProcessingException, ExceptionHandler {
-        String output = cmdHandler.getCommodityById(1);
-        String expectedOutput = "\"data\": {\"id\": 1, \"name\": \"Headphone\", \"providerId\": 3, \"price\": 35000, " +
-                "\"categories\": [\"Technology\", \"Phone\"], \"rating\": 8.8}";
+    public void testAddToBuyListIfUserDoesNotExist() throws JsonProcessingException, ExceptionHandler {
+        String output = cmdHandler.addToUserBuyList("{\"username\": \"user2\", \"commodityId\": 1}");
+        String expectedOutput = "{\"success\" : false, \"data\" : \"Couldn't find user with the given Username!\"}";
         assertEquals(expectedOutput, output);
     }
-
     @Test
-    public void testCommodityAlreadyExistedInBuyListUser() throws JsonProcessingException, ExceptionHandler {
-        String output = cmdHandler.getCommodityById(1);
-        String expectedOutput = "\"data\": {\"id\": 1, \"name\": \"Headphone\", \"providerId\": 3, \"price\": 35000, " +
-                "\"categories\": [\"Technology\", \"Phone\"], \"rating\": 8.8}";
+    public void testAddToBuyListIfCommodityDoesNotExist() throws JsonProcessingException, ExceptionHandler {
+        String output = cmdHandler.addToUserBuyList("{\"username\": \"user1\", \"commodityId\": 4}");
+        String expectedOutput = "{\"success\" : false, \"data\" : \"Couldn't find commodity with the given Id!\"}";
         assertEquals(expectedOutput, output);
     }
-
     @Test
-    public void testAddToBuyListNotExistedCommodity() throws JsonProcessingException, ExceptionHandler {
-        String output = cmdHandler.getCommodityById(1);
-        String expectedOutput = "\"data\": {\"id\": 1, \"name\": \"Headphone\", \"providerId\": 3, \"price\": 35000, " +
-                "\"categories\": [\"Technology\", \"Phone\"], \"rating\": 8.8}";
+    public void testAddToBuyListIfCommodityAlreadyExistedInBuyListUser() throws JsonProcessingException, ExceptionHandler {
+        String dummy = cmdHandler.addToUserBuyList("{\"username\": \"user1\", \"commodityId\": 1}");
+        String output = cmdHandler.addToUserBuyList("{\"username\": \"user1\", \"commodityId\": 1}");
+
+        String expectedOutput = "{\"success\" : false, \"data\" :\"Commodity already exists in your BuyList!\"}";
+
         assertEquals(expectedOutput, output);
     }
-
     @Test
     public void testAddToBuyListOutOfStockCommodity() throws JsonProcessingException, ExceptionHandler {
-        String output = cmdHandler.getCommodityById(1);
-        String expectedOutput = "\"data\": {\"id\": 1, \"name\": \"Headphone\", \"providerId\": 3, \"price\": 35000, " +
-                "\"categories\": [\"Technology\", \"Phone\"], \"rating\": 8.8}";
+        String output = cmdHandler.addToUserBuyList("{\"username\": \"user1\", \"commodityId\": 4}");
+        String expectedOutput = "{\"success\" : false, \"data\" : \"Commodity out of stock!\"}";
         assertEquals(expectedOutput, output);
     }
-
     @Test
     public void testAddToBuyListSuccessful() throws JsonProcessingException, ExceptionHandler {
-        String output = cmdHandler.getCommodityById(1);
-        String expectedOutput = "\"data\": {\"id\": 1, \"name\": \"Headphone\", \"providerId\": 3, \"price\": 35000, " +
-                "\"categories\": [\"Technology\", \"Phone\"], \"rating\": 8.8}";
+        String output = cmdHandler.addToUserBuyList("{\"username\": \"user1\", \"commodityId\": 1}");
+        String expectedOutput = "{\"success\" : true, \"data\" : \"Commodity added to user buy list successfully\"}";
         assertEquals(expectedOutput, output);
     }
 }

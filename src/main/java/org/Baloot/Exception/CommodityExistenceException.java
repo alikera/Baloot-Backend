9922e1e2ef4d6e8557e2f769d17d1069368c47d8
alikera <alikeramati79@gmail.com>
2023-03-01 15:49:32 +0330

    Throwing exceptions bugs fixed

---
+++ src/main/java/org/Baloot/Exception/CommodityExistenceException.java
@@ -0,0 +1,7 @@
+package org.Baloot.Exception;
+
+public class CommodityExistenceException extends ExceptionHandler {
+    public CommodityExistenceException(String message) {
+        super(message);
+    }
+}

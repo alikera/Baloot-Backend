4bd18fb866b7c23933f8485df185fd9fac6522c9
Ali Pakdel <aliipakdel98@gmail.com>
2023-03-17 23:52:26 +0330

    Reformat

--- src/main/java/org/Baloot/Main.java
+++ src/main/java/org/Baloot/Main.java
@@ -1,11 +1,10 @@
 package org.Baloot;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.Baloot.Database.Database;
 import org.Baloot.Exception.ExceptionHandler;
+import org.Baloot.Services.DataGetter;
+import org.Baloot.Services.RequestHandler;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException, ExceptionHandler {
@@ -15,16 +14,9 @@ public class Main {
         dataGetter.getDataFromServer(db);
 
         Baloot baloot = new Baloot(db);
-
         RequestHandler rh = new RequestHandler(baloot);
         rh.getRequest();
-//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-//        String line;
-//        CommandHandler ch = new CommandHandler();
-//        while ((line = reader.readLine()) != null) {
-//            String[] seperatedLine = line.split(" ", 2);
-//            ch.executeCommands(seperatedLine);
-//        }
+
     }
 }
 

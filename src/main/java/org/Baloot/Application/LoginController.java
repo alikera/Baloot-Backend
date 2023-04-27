0f6d888427361eba4312762a666e675f5c4250cc
Ali Pakdel <aliipakdel98@gmail.com>
2023-04-27 14:32:26 +0330

    Register API append

---
+++ src/main/java/org/Baloot/Application/LoginController.java
@@ -0,0 +1,29 @@
+package org.Baloot.Application;
+
+import org.Baloot.Baloot;
+import org.Baloot.Entities.Date;
+import org.Baloot.Entities.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:8080")
+public class LoginController {
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public User login(@RequestBody Map<String, String> body) {
+        String username = body.get("username");
+        String password = body.get("password");
+        return Baloot.getBaloot().userManager.login(username, password);
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public User register(@RequestBody Map<String, String> body) {
+        String username = body.get("username");
+        String password = body.get("password");
+        String email = body.get("email");
+        String address = body.get("address");
+        String date = body.get("birthDate");
+        return Baloot.getBaloot().userManager.registerNewUser(username, password, email, address, date);
+    }
+}

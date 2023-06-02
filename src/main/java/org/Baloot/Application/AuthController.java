a9eb11f67750ec19cb33ed5df3dfe5131ab47575
alikera <alikeramati79@gmail.com>
2023-06-02 12:16:04 +0330

    JWT bugs fixed

--- src/main/java/org/Baloot/Application/AuthController.java
+++ src/main/java/org/Baloot/Application/AuthController.java
@@ -2,6 +2,10 @@ package org.Baloot.Application;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.xml.bind.DatatypeConverter;
 import org.Baloot.Baloot;
 import org.Baloot.Exception.DuplicateEmailException;
 import org.Baloot.Exception.DuplicateUsernameException;
@@ -19,9 +23,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.crypto.spec.SecretKeySpec;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.security.Key;
 import java.sql.SQLException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -31,21 +40,56 @@ import java.util.Map;
 public class AuthController {
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public ResponseEntity<?> login(@RequestBody Map<String, String> body) {
-        String username = body.get("username");
+        String userEmail = body.get("email");
         String password = body.get("password");
         System.out.println("here");
         try {
-            Baloot.getBaloot().userManager.login(username, password);
-            return ResponseEntity.status(HttpStatus.OK).build();
-        } catch (UserNotFoundException e) {
+            String username = Baloot.getBaloot().userManager.login(userEmail, password);
+            String token = generateJwtToken(username);
+            Map<String, String> response = new HashMap<>();
+            response.put("token", token);
+            return ResponseEntity.ok().body(response);
+        }
+        catch (UserNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        } catch (IncorrectPasswordException e) {
+        }
+        catch (IncorrectPasswordException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
             return ResponseEntity.status(408).body("Database Error");
         }
+    }
+
+    private String generateJwtToken(String username) {
+        String SECRET_KEY = "baloot2023";
+        // Create claims for JWT payload
+        Claims claims = Jwts.claims();
+        claims.setSubject(username);
+        claims.setIssuedAt(new Date());
+
+        // Set expiration time to 1 day from now
+        Date expirationDate = new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000);
+        claims.setExpiration(expirationDate);
+
+        byte[] keyBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY+SECRET_KEY+SECRET_KEY+SECRET_KEY+SECRET_KEY);
+        Key key = new SecretKeySpec(keyBytes, SignatureAlgorithm.HS256.getJcaName());
+
+        // Generate JWT token with signature
+        return Jwts.builder()
+                .setIssuer("baloot")
+                .setExpiration(Date.from(Instant.now().plus(1, ChronoUnit.DAYS)))
+                .setIssuedAt(Date.from(Instant.now()))
+                .claim("username", username)
+                .signWith(key)
+                .compact();
+    }
+    private static Key getKey() {
+        String SECRET_KEY = "baloot2023";
+
+        byte[] keyBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY+SECRET_KEY+SECRET_KEY+SECRET_KEY+SECRET_KEY);
+        return new SecretKeySpec(keyBytes, SignatureAlgorithm.HS256.getJcaName());
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
